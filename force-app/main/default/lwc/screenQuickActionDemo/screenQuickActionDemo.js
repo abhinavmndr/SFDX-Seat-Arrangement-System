c06d79a799723add55c8ad93cb165516b1bc1f60
Abhinav Manandhar <mndrabhinav.trailhead@gmail.com>
2025-12-30 18:50:27 +0545

    Added record pages as well as modified test classes

--- force-app/main/default/lwc/screenQuickActionDemo/screenQuickActionDemo.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class ScreenQuickActionDemo extends LightningElement {
-    handleClose() {
-        this.dispatchEvent(new CustomEvent('close'));
-    }
-}
